e43841622bdb9779cb93039d04b71ac54138c87f
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-09-07 15:49:42 -0700

    Listen for 'submit' events on the journal entry form and build event handler for submission

--- js/main.js
+++ js/main.js
@@ -8,3 +8,25 @@ function addPhoto(event) {
 }
 
 $photoUrl.addEventListener('input', addPhoto);
+
+var $entryForm = document.querySelector('form');
+
+function formSubmit(event) {
+  event.preventDefault();
+  var formValues = {};
+
+  formValues.title = $entryForm.elements.title.value;
+  formValues.photoUrl = $entryForm.elements.photoUrl.value;
+  formValues.notes = $entryForm.elements.notes.value;
+
+  formValues.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+
+  data.entries.unshift(formValues);
+
+  $previewImg.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  $entryForm.reset();
+}
+
+$entryForm.addEventListener('submit', formSubmit);
